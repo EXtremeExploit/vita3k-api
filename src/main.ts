860f20407ba2882fcdcb31f965ba00f49a7f017a
EXtremeExploit <pedro.montes.alcalde@gmail.com>
2023-10-11 12:16:15 -0300

    Paralelize both lists gathering and optimize comparasion

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-import lodash from 'lodash';
-
 import { router } from './route';
 import { GetGithubIssues, LOG, updateTimestamp } from './utils';
 import { Env, GameEntry } from './types';
@@ -21,26 +19,48 @@ export default {
 		for (const type of lists) {
 			LOG(`Caching ${type} list...`);
 			await updateTimestamp(env, type);
-			const list = await GetGithubIssues(env, type);
-			LOG(`Github list ${type} has ${list.length} entries`);
 
-			let cachedList = (await env.DB.prepare('SELECT `name`,`titleId`,`status`,`color`,`issueId` FROM list WHERE type = ? ORDER BY titleId ASC, issueId ASC').bind(type).all()).results as unknown as GameEntry[];
-			// Only sort the github issues list as the cached list is already sorted by the query
-			list.sort((a, b) => {
-				if (a.titleId.toLowerCase() < b.titleId.toLowerCase()) return -1;
-				if (a.titleId.toLowerCase() > b.titleId.toLowerCase()) return 1;
-				return a.issueId - b.issueId; // This will NEVER be the same, its the primary key
-			});
+			const [ghList, cachedListResult] = await Promise.all([
+				GetGithubIssues(env, type),
+				env.DB.prepare('SELECT `name`,`titleId`,`status`,`color`,`issueId` FROM list WHERE type = ? SORT BY titleId ASC, issueId ASC').bind(type).all()
+			]);
+			LOG(`Github list ${type} has ${ghList.length} entries`);
 
-			const areEqual = lodash.isEqual(list, cachedList)
-			if (areEqual) {
-				LOG('Lists are equal, nothing to do');
-				return;
+			const cachedList = cachedListResult.results as unknown as GameEntry[];
+
+			if (cachedList.length == ghList.length) {
+				// Only sort the github issues list as the cached list is already sorted by the query
+				ghList.sort((a, b) => {
+					if (a.titleId.toLowerCase() < b.titleId.toLowerCase()) return -1;
+					if (a.titleId.toLowerCase() > b.titleId.toLowerCase()) return 1;
+					return a.issueId - b.issueId; // This will NEVER be the same, its the primary key
+				});
+				// Order of elements should be EXACTLY the same, and the lists should be parallel
+				// Compare them in the for below
+
+				let areEqual = true;
+				for (let i = 0; i < ghList.length; i++) {
+					const ghIssue = ghList[i];
+					const cachedIssue = cachedList[i];
+
+					if (ghIssue.titleId != cachedIssue.titleId) { areEqual = false; break; }
+					if (ghIssue.name != cachedIssue.name) { areEqual = false; break; }
+					if (ghIssue.status != cachedIssue.status) { areEqual = false; break; }
+					if (ghIssue.issueId != cachedIssue.issueId) { areEqual = false; break; }
+					if (ghIssue.color != cachedIssue.color) { areEqual = false; break; }
+				}
+
+				if (areEqual) {
+					LOG('Lists are equal, nothing to do');
+					return;
+				} else {
+					LOG('Lists are not equal, reinserting all entries');
+				}
 			}
 
 			const batch: D1PreparedStatement[] = [];
 			batch.push(env.DB.prepare('DELETE FROM list WHERE type = ?').bind(type));
-			list.forEach((e) => {
+			ghList.forEach((e) => {
 				batch.push(env.DB.prepare('INSERT INTO list (`type`,`name`,`titleId`,`status`,`color`,`issueId`) VALUES (?,?,?,?,?,?)')
 					.bind(type, e.name, e.titleId, e.status, e.color, e.issueId));
 			});
